5d0e8718471222a2ba6c1d6a30c21ec746545ad5
Raven Manalastas <rmanalastas@rrc.ca>
2025-02-02 01:43:38 -0600

    feat: integrate Swagger for API documentation.

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,12 @@ import morgan from "morgan"; // Import Morgan
 
 // Import the item routes from the new routes file
 import itemRoutes from "./api/v1/routes/itemRoutes";
+import setupSwagger from "../config/swagger"; // Import Swagger setup
 
 const app: Express = express();
+
+// Setup Swagger
+setupSwagger(app);
 
 // Use Morgan for HTTP request logging
 app.use(morgan("combined"));
